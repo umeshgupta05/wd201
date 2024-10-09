34f32f92347a0771571de4b4510d0c82953a01f3
umeshgupta05 <saiumeshgupta@gmail.com>
2024-10-09 09:01:06 +0530

    Update eslint.config.mjs

--- todo-cli/eslint.config.mjs
+++ todo-cli/eslint.config.mjs
@@ -1,19 +1,27 @@
-{
-  "env": {
-    "node": true,
-    // "browser": true,
-    "commonjs": true,
-    "es2021": true,
-    "jest": true
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  pluginJs.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: globals.browser
+    },
+    env: {
+      node: true, // Add this line to define the Node.js environment
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "no-undef": "off"
+    },
+    ignores: [
+      "models/**/*.js",
+      "__tests__/**/*.js",
+      "addTodo.js",
+      "completeTodo.js",
+      "listTodos.js"
+    ],
   },
-  "extends": "eslint:recommended",
-  "parserOptions": {
-    "ecmaVersion": "latest"
-  },
-  "rules": {
-    "no-unused-vars": [
-      "error",
-      { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }
-    ]
-  }
-}
+];
